dde31219a8785fed84b9ef3563a8470af626f182
EC2 Default User <ec2-user@ip-172-31-66-241.ec2.internal>
2021-10-26 06:03:13 +0000

    Initial Commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,21 +10,19 @@ class UsersController < ApplicationController
   def index
     @users = User.all
     @book = Book.new
+    @user = current_user
   end
 
   def edit
     @user = User.find(params[:id])
-    if @user == current_user
-      render 'edit'
-    else
-      redirect_to user_path(current_user)
-    end
+   
+    
   end
 
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      redirect_to user_path(@user.id), notice: "You have updated user successfully."
+      redirect_to user_path(current_user), notice: "You have updated user successfully."
     else
       render "edit"
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,20 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'homes#top'
-  
+
   resources :users,only: [:show,:index,:edit,:update]
   resources :books do
     resource :favorites,only: [:create, :destroy]
      resources :book_comments,only: [:create, :destroy]
    end
 
+    resources :users,only: [:show,:index,:edit,:update] do
+     resource :relationships,only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
+
   get 'home/about' => 'homes#about'
-  
+  get 'searches/search'
+
 end
